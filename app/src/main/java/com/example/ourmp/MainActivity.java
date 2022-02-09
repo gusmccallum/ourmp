4ab03b11092301903f1474809bac2af89459ceed
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2022-02-09 18:41:57 -0500

    Search feature

--- app/src/main/java/com/example/ourmp/MainActivity.java
+++ app/src/main/java/com/example/ourmp/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.ourmp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button findMP;
+    Button btn_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
                 Intent myIntent = new Intent();
                 myIntent.setClass(getApplicationContext(), LocationActivity.class);
                 startActivity(myIntent);
+            }
+        });
+
+        btn_search = (Button) findViewById(R.id.btn_search);
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Search.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/ourmp/Search.java
@@ -0,0 +1,57 @@
+package com.example.ourmp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Search extends Activity {
+
+    SearchView mySearchView;
+    ListView myList;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mySearchView = (SearchView)findViewById(R.id.searchView);
+        myList = (ListView) findViewById(R.id.myList);
+
+        // Add items to list
+        list = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.mps)));
+
+        adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, list);
+
+        myList.setAdapter(adapter);
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Show results
+                myList.setVisibility(View.VISIBLE);
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+
+}
