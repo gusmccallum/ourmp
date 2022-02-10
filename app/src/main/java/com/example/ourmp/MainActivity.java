1af775e64dacd0e088eaeafc072a1f6a5d817e18
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2022-02-09 21:16:07 -0500

    Search feature (#30)

--- app/src/main/java/com/example/ourmp/MainActivity.java
+++ app/src/main/java/com/example/ourmp/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.ourmp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button findMP;
+    Button btn_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findMP = (Button) findViewById(R.id.findMP);
         findMP.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
                 Intent myIntent = new Intent();
                 myIntent.setClass(getApplicationContext(), LocationActivity.class);
                 startActivity(myIntent);
+            }
+        });
+
+        btn_search = (Button) findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Search.class);
+                startActivity(intent);
             }
         });
     }
