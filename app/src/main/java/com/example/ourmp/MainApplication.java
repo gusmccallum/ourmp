a6885993dbf79a32f953088b43970cd9ff81b177
gusmccallum <gusmccallum@hotmail.com>
2022-02-10 11:19:38 -0500

    Working on Realm Insert

--- app/src/main/java/com/example/ourmp/MainApplication.java
+++ app/src/main/java/com/example/ourmp/MainApplication.java
@@ -2,7 +2,12 @@ package com.example.ourmp;
 
 import android.app.Application;
 
+import io.realm.Realm;
+
 public class MainApplication extends Application {
+
+    Realm.init(this);
+
 
     private DBManager dbManager = new DBManager();
     public DBManager getDbManager(){return dbManager;}
